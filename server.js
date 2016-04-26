66d8a3bb993b9487be54845ca0132684faf612d3
Magnus Stenqvist <magnus.p.stenqvist@gmail.com>
2016-04-26 14:50:15 +0200

    Added socket.io to project

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var http = require('http');
 
-app.get('/', function (req, res) {
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+server.listen(8000);
+
+io.set("origins", "*:*");
+
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.use("/", express.static(__dirname + "/"));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+io.on('connection', function (socket) {
+  console.log("Connected");
+	socket.emit('foo', 42);
 });
